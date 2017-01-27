5dfcfbbe91cf8a537eff56e4d79974c8e79454f3
Brett V. Forsgren <brett.forsgren@outlook.com>
2017-01-27 12:28:09 -0800

    additional DXB reader test

--- src/IxMilia.Dxf.Test/DxfReaderWriterTests.cs
+++ src/IxMilia.Dxf.Test/DxfReaderWriterTests.cs
@@ -15,6 +15,8 @@ namespace IxMilia.Dxf.Test
 {
     public class DxfReaderWriterTests : AbstractDxfTests
     {
+        private readonly byte[] DxbSentinel = new byte[] { (byte)'A', (byte)'u', (byte)'t', (byte)'o', (byte)'C', (byte)'A', (byte)'D', (byte)' ', (byte)'D', (byte)'X', (byte)'B', (byte)' ', (byte)'1', (byte)'.', (byte)'0', (byte)'\r', (byte)'\n', 0x1A, 0x0 };
+
         [Fact]
         public void BinaryReaderTest()
         {
@@ -31,11 +33,8 @@ namespace IxMilia.Dxf.Test
         [Fact]
         public void ReadDxbTest()
         {
-            var data = new byte[]
+            var data = DxbSentinel.Concat(new byte[]
             {
-                // DXB sentinel
-                (byte)'A', (byte)'u', (byte)'t', (byte)'o', (byte)'C', (byte)'A', (byte)'D', (byte)' ', (byte)'D', (byte)'X', (byte)'B', (byte)' ', (byte)'1', (byte)'.', (byte)'0', (byte)'\r', (byte)'\n', 0x1A, 0x0,
-
                 // color
                 136, // type specifier for new color
                 0x01, 0x00, // color index 1
@@ -45,29 +44,27 @@ namespace IxMilia.Dxf.Test
                 0x01, 0x00, // P1.X = 0x0001
                 0x02, 0x00, // P1.Y = 0x0002
                 0x03, 0x00, // P1.Z = 0x0003
-                0x04, 0x00, // P1.X = 0x0004
-                0x05, 0x00, // P1.Y = 0x0005
-                0x06, 0x00, // P1.Z = 0x0006
+                0x04, 0x00, // P2.X = 0x0004
+                0x05, 0x00, // P2.Y = 0x0005
+                0x06, 0x00, // P2.Z = 0x0006
 
-                // null terminator
-                0x0
-            };
-            var stream = new MemoryStream(data);
-            var file = DxfFile.Load(stream);
-            var line = (DxfLine)file.Entities.Single();
-            Assert.Equal(1, line.Color.RawValue);
-            Assert.Equal(new DxfPoint(1, 2, 3), line.P1);
-            Assert.Equal(new DxfPoint(4, 5, 6), line.P2);
+                0x0                 // null terminator
+            }).ToArray();
+            using (var stream = new MemoryStream(data))
+            {
+                var file = DxfFile.Load(stream);
+                var line = (DxfLine)file.Entities.Single();
+                Assert.Equal(1, line.Color.RawValue);
+                Assert.Equal(new DxfPoint(1, 2, 3), line.P1);
+                Assert.Equal(new DxfPoint(4, 5, 6), line.P2);
+            }
         }
 
         [Fact]
         public void ReadDxbNoLengthOrPositionStreamTest()
         {
-            var data = new byte[]
+            var data = DxbSentinel.Concat(new byte[]
             {
-                // DXB sentinel
-                (byte)'A', (byte)'u', (byte)'t', (byte)'o', (byte)'C', (byte)'A', (byte)'D', (byte)' ', (byte)'D', (byte)'X', (byte)'B', (byte)' ', (byte)'1', (byte)'.', (byte)'0', (byte)'\r', (byte)'\n', 0x1A, 0x0,
-
                 // color
                 136, // type specifier for new color
                 0x01, 0x00, // color index 1
@@ -77,13 +74,12 @@ namespace IxMilia.Dxf.Test
                 0x01, 0x00, // P1.X = 0x0001
                 0x02, 0x00, // P1.Y = 0x0002
                 0x03, 0x00, // P1.Z = 0x0003
-                0x04, 0x00, // P1.X = 0x0004
-                0x05, 0x00, // P1.Y = 0x0005
-                0x06, 0x00, // P1.Z = 0x0006
+                0x04, 0x00, // P2.X = 0x0004
+                0x05, 0x00, // P2.Y = 0x0005
+                0x06, 0x00, // P2.Z = 0x0006
 
-                // null terminator
-                0x0
-            };
+                0x0 // null terminator
+            }).ToArray();
             using (var ms = new MemoryStream(data))
             using (var stream = new StreamWithNoLengthOrPosition(ms))
             {
@@ -92,6 +88,36 @@ namespace IxMilia.Dxf.Test
                 Assert.Equal(1, line.Color.RawValue);
                 Assert.Equal(new DxfPoint(1, 2, 3), line.P1);
                 Assert.Equal(new DxfPoint(4, 5, 6), line.P2);
+            }
+        }
+
+        [Fact]
+        public void ReadDxbPolylineTest()
+        {
+            var data = DxbSentinel.Concat(new byte[]
+            {
+                19, // polyline
+                0x00, 0x00, // is closed = false
+
+                20, // vertex
+                0x01, 0x00, // x
+                0x02, 0x00, // y
+
+                20, // vertex
+                0x03, 0x00, // x
+                0x04, 0x00, // y
+
+                17, // seqend
+
+                0x00 // null terminator
+            }).ToArray();
+            using (var stream = new MemoryStream(data))
+            {
+                var file = DxfFile.Load(stream);
+                var poly = (DxfPolyline)file.Entities.Single();
+                Assert.Equal(2, poly.Vertices.Count);
+                Assert.Equal(new DxfPoint(1.0, 2.0, 0.0), poly.Vertices[0].Location);
+                Assert.Equal(new DxfPoint(3.0, 4.0, 0.0), poly.Vertices[1].Location);
             }
         }
 

c0ce0749c7d6806748863354ed548cd023c9ae1f
Brett V. Forsgren <brett.forsgren@outlook.com>
2017-08-18 14:53:54 -0700

    explicitly generate code with build script

--- build-and-test.cmd
+++ build-and-test.cmd
@@ -1,5 +1,34 @@
-set TEST_PROJECT=.\src\IxMilia.Dxf.Test\IxMilia.Dxf.Test.csproj
-dotnet restore %TEST_PROJECT%
-if errorlevel 1 exit /b 1
-dotnet test %TEST_PROJECT%
+@echo off
 
+:: run code generator
+set GENERATOR_DIR=%~dp0src\IxMilia.Dxf.Generator
+set LIBRARY_DIR=%~dp0src\IxMilia.Dxf
+pushd "%GENERATOR_DIR%"
+dotnet restore
+if errorlevel 1 goto error
+dotnet build
+if errorlevel 1 goto error
+dotnet run "--entityDir=%LIBRARY_DIR%\Entities\Generated" "--objectDir=%LIBRARY_DIR%\Objects\Generated" "--sectionDir=%LIBRARY_DIR%\Sections\Generated" "--tableDir=%LIBRARY_DIR%\Tables\Generated"
+if errorlevel 1 goto error
+popd
+
+:: build library
+set LIBRARY_PROJECT=%LIBRARY_DIR%\IxMilia.Dxf.csproj
+dotnet restore "%LIBRARY_PROJECT%"
+if errorlevel 1 goto error
+dotnet build "%LIBRARY_PROJECT%"
+if errorlevel 1 goto error
+
+:: build and run tests
+set TEST_PROJECT=%~dp0src\IxMilia.Dxf.Test\IxMilia.Dxf.Test.csproj
+dotnet restore "%TEST_PROJECT%"
+if errorlevel 1 goto error
+dotnet build "%TEST_PROJECT%"
+if errorlevel 1 goto error
+dotnet test "%TEST_PROJECT%"
+if errorlevel 1 goto error
+goto :eof
+
+:error
+echo Error building project.
+exit /b 1
--- build-and-test.sh
+++ build-and-test.sh
@@ -1,6 +1,23 @@
 #!/bin/sh -e
 
-TEST_PROJECT=./src/IxMilia.Dxf.Test/IxMilia.Dxf.Test.csproj
-dotnet restore $TEST_PROJECT
-dotnet test $TEST_PROJECT
+_SCRIPT_DIR="$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )"
 
+# run code generator
+GENERATOR_DIR=$_SCRIPT_DIR/src/IxMilia.Dxf.Generator
+LIBRARY_DIR=$_SCRIPT_DIR/src/IxMilia.Dxf
+cd $GENERATOR_DIR
+dotnet restore
+dotnet build
+dotnet run "--entityDir=$LIBRARY_DIR/Entities/Generated" "--objectDir=$LIBRARY_DIR/Objects/Generated" "--sectionDir=$LIBRARY_DIR/Sections/Generated" "--tableDir=$LIBRARY_DIR/Tables/Generated"
+cd -
+
+# build library
+LIBRARY_PROJECT=$LIBRARY_DIR/IxMilia.Dxf.csproj
+dotnet restore "$LIBRARY_PROJECT"
+dotnet build "$LIBRARY_PROJECT"
+
+# build and run tests
+TEST_PROJECT=$_SCRIPT_DIR/src/IxMilia.Dxf.Test/IxMilia.Dxf.Test.csproj
+dotnet restore "$TEST_PROJECT"
+dotnet build "$TEST_PROJECT"
+dotnet test "$TEST_PROJECT"

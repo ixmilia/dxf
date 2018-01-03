a32e8eead79b41e3be0282846e8d892071ed4ea5
Brett V. Forsgren <brett.forsgren@outlook.com>
2018-01-03 15:29:09 -0800

    tag official binaries with git SHA

--- src/create-package.cmd
+++ src/create-package.cmd
@@ -1,5 +1,6 @@
-set PROJECT=.\IxMilia.Dxf\IxMilia.Dxf.csproj
+@echo off
+
+set PROJECT=%~dp0IxMilia.Dxf\IxMilia.Dxf.csproj
 dotnet restore %PROJECT%
 if errorlevel 1 exit /b 1
-dotnet pack --include-symbols --include-source --configuration Release %PROJECT%
-
+dotnet pack --include-symbols --include-source --configuration Release %PROJECT% /p:OfficialBuild=true
--- src/create-package.sh
+++ src/create-package.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-PROJECT=./IxMilia.Dxf/IxMilia.Dxf.csproj
+_SCRIPT_DIR="$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )"
+PROJECT=$_SCRIPT_DIR/IxMilia.Dxf/IxMilia.Dxf.csproj
 dotnet restore $PROJECT
-dotnet pack --include-symbols --include-source --configuration Release $PROJECT
-
+dotnet pack --include-symbols --include-source --configuration Release $PROJECT /p:OfficialBuild=true

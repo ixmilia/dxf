992424f8611581277268b2fc65182d0ce6337640
Brett V. Forsgren <brett.forsgren@outlook.com>
2018-01-29 17:05:00 -0800

    only build net45 on Windows

--- src/create-package.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -e
-
-_SCRIPT_DIR="$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )"
-PROJECT=$_SCRIPT_DIR/IxMilia.Dxf/IxMilia.Dxf.csproj
-dotnet restore $PROJECT
-dotnet pack --include-symbols --include-source --configuration Release $PROJECT /p:OfficialBuild=true

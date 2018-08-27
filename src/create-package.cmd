3ca7997e1713f158a6e9d43e80259046817108b3
Brett V. Forsgren <brett.forsgren@outlook.com>
2018-08-27 10:32:05 -0700

    enable SourceLink

--- src/create-package.cmd
+++ src/create-package.cmd
@@ -3,4 +3,4 @@
 set PROJECT=%~dp0IxMilia.Dxf\IxMilia.Dxf.csproj
 dotnet restore %PROJECT%
 if errorlevel 1 exit /b 1
-dotnet pack --include-symbols --include-source --configuration Release %PROJECT% /p:OfficialBuild=true
+dotnet pack --configuration Release %PROJECT% /p:OfficialBuild=true

556eced5d537e76e4bd1ee0ce73056064dec4c0c
Brett V. Forsgren <brett.forsgren@outlook.com>
2018-11-29 15:28:20 -0800

    build everything to `Artifacts/` with parameterized version

--- src/create-package.cmd
+++ src/create-package.cmd
@@ -1,6 +1,19 @@
 @echo off
 
-set PROJECT=%~dp0IxMilia.Dxf\IxMilia.Dxf.csproj
+set PROJECT_NAME=IxMilia.Dxf
+set CONFIGURATION=Release
+set PROJECT=%~dp0\%PROJECT_NAME%\%PROJECT_NAME%.csproj
+set /p VERSION=<"%~dp0..\version.txt"
+set PACKAGE_FILE=%~dp0..\Artifacts\%PROJECT_NAME%\bin\%CONFIGURATION%\%PROJECT_NAME%.%VERSION%.nupkg
+set FINAL_PACKAGE_FILE=%~dp0..\Artifacts\%PROJECT_NAME%\bin\%CONFIGURATION%\%PROJECT_NAME%.nupkg
+
+del "%PACKAGE_FILE%"
+del "%FINAL_PACKAGE_FILE%"
+
 dotnet restore %PROJECT%
 if errorlevel 1 exit /b 1
-dotnet pack --configuration Release %PROJECT%
+
+dotnet pack --configuration %CONFIGURATION% %PROJECT%
+if errorlevel 1 exit /b 1
+
+copy "%PACKAGE_FILE%" "%FINAL_PACKAGE_FILE%"

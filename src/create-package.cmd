bcbf364be2a38391aa2e4d974dc9da4ef812dc91
Brett V. Forsgren <brett.forsgren@outlook.com>
2018-11-30 09:27:20 -0800

    build package to separate directory

--- src/create-package.cmd
+++ src/create-package.cmd
@@ -3,17 +3,13 @@
 set PROJECT_NAME=IxMilia.Dxf
 set CONFIGURATION=Release
 set PROJECT=%~dp0\%PROJECT_NAME%\%PROJECT_NAME%.csproj
-set /p VERSION=<"%~dp0..\version.txt"
-set PACKAGE_FILE=%~dp0..\Artifacts\%PROJECT_NAME%\bin\%CONFIGURATION%\%PROJECT_NAME%.%VERSION%.nupkg
-set FINAL_PACKAGE_FILE=%~dp0..\Artifacts\%PROJECT_NAME%\bin\%CONFIGURATION%\%PROJECT_NAME%.nupkg
+set OUTPUT_PATH=%~dp0..\Artifacts\NuGet
 
-del "%PACKAGE_FILE%"
-del "%FINAL_PACKAGE_FILE%"
-
-dotnet restore %PROJECT%
+dotnet restore "%PROJECT%"
 if errorlevel 1 exit /b 1
 
-dotnet pack --configuration %CONFIGURATION% %PROJECT%
+dotnet build "%PROJECT%" --configuration %CONFIGURATION%
 if errorlevel 1 exit /b 1
 
-copy "%PACKAGE_FILE%" "%FINAL_PACKAGE_FILE%"
+dotnet pack --no-restore --no-build --configuration %CONFIGURATION% --output "%OUTPUT_PATH%" "%PROJECT%"
+if errorlevel 1 exit /b 1

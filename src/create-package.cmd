638e08d8eb3bd6351b890252bce01b2c4ffc61c0
Brett V. Forsgren <brett.forsgren@outlook.com>
2018-12-07 12:58:38 -0800

    tweak artifacts path

--- src/create-package.cmd
+++ src/create-package.cmd
@@ -3,7 +3,6 @@
 set PROJECT_NAME=IxMilia.Dxf
 set CONFIGURATION=Release
 set PROJECT=%~dp0\%PROJECT_NAME%\%PROJECT_NAME%.csproj
-set OUTPUT_PATH=%~dp0..\Artifacts\NuGet
 
 dotnet restore "%PROJECT%"
 if errorlevel 1 exit /b 1
@@ -11,5 +10,5 @@ if errorlevel 1 exit /b 1
 dotnet build "%PROJECT%" --configuration %CONFIGURATION%
 if errorlevel 1 exit /b 1
 
-dotnet pack --no-restore --no-build --configuration %CONFIGURATION% --output "%OUTPUT_PATH%" "%PROJECT%"
+dotnet pack --no-restore --no-build --configuration %CONFIGURATION% "%PROJECT%"
 if errorlevel 1 exit /b 1

51d39b23967530cc2659639e98699a9f167bda57
Brett V. Forsgren <brett.forsgren@outlook.com>
2019-05-10 15:00:16 -0700

    refactor build

--- build-and-test.cmd
+++ build-and-test.cmd
@@ -1,4 +1,27 @@
 @echo off
+setlocal
+
+set configuration=Debug
+set runtests=true
+
+:parseargs
+if "%1" == "" goto argsdone
+if /i "%1" == "-c" (
+    set configuration=%2
+    shift
+    shift
+    goto parseargs
+)
+if /i "%1" == "-notest" (
+    set runtests=false
+    shift
+    goto parseargs
+)
+
+echo Unsupported argument: %1
+goto error
+
+:argsdone
 
 :: run code generator
 set GENERATOR_DIR=%~dp0src\IxMilia.Dxf.Generator
@@ -6,9 +29,9 @@ set LIBRARY_DIR=%~dp0src\IxMilia.Dxf
 pushd "%GENERATOR_DIR%"
 dotnet restore
 if errorlevel 1 goto error
-dotnet build
+dotnet build -c %configuration%
 if errorlevel 1 goto error
-dotnet run "%LIBRARY_DIR%"
+dotnet run -c %configuration% --no-restore --no-build -- "%LIBRARY_DIR%"
 if errorlevel 1 goto error
 popd
 
@@ -16,17 +39,22 @@ popd
 set LIBRARY_PROJECT=%LIBRARY_DIR%\IxMilia.Dxf.csproj
 dotnet restore "%LIBRARY_PROJECT%"
 if errorlevel 1 goto error
-dotnet build "%LIBRARY_PROJECT%"
+dotnet build "%LIBRARY_PROJECT%" -c %configuration%
 if errorlevel 1 goto error
 
-:: build and run tests
+:: build tests
 set TEST_PROJECT=%~dp0src\IxMilia.Dxf.Test\IxMilia.Dxf.Test.csproj
 dotnet restore "%TEST_PROJECT%"
 if errorlevel 1 goto error
-dotnet build "%TEST_PROJECT%"
+dotnet build "%TEST_PROJECT%" -c %configuration%
 if errorlevel 1 goto error
-dotnet test "%TEST_PROJECT%"
-if errorlevel 1 goto error
+
+:: run tests
+if /i "%runtests%" == "true" (
+    dotnet test "%TEST_PROJECT%" -c %configuration% --no-restore --no-build
+    if errorlevel 1 goto error
+)
+
 goto :eof
 
 :error
--- build-and-test.sh
+++ build-and-test.sh
@@ -2,22 +2,46 @@
 
 _SCRIPT_DIR="$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )"
 
+CONFIGURATION=Debug
+RUNTESTS=true
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --configuration|-c)
+      CONFIGURATION=$2
+      shift
+      ;;
+    --notest)
+      RUNTESTS=false
+      ;;
+    *)
+      echo "Invalid argument: $1"
+      exit 1
+      ;;
+  esac
+  shift
+done
+
 # run code generator
 GENERATOR_DIR=$_SCRIPT_DIR/src/IxMilia.Dxf.Generator
 LIBRARY_DIR=$_SCRIPT_DIR/src/IxMilia.Dxf
 cd $GENERATOR_DIR
 dotnet restore
-dotnet build
-dotnet run "$LIBRARY_DIR"
+dotnet build -c $CONFIGURATION
+dotnet run -c $CONFIGURATION --no-restore --no-build -- "$LIBRARY_DIR"
 cd -
 
 # build library
 LIBRARY_PROJECT=$LIBRARY_DIR/IxMilia.Dxf.csproj
 dotnet restore "$LIBRARY_PROJECT"
-dotnet build "$LIBRARY_PROJECT"
+dotnet build "$LIBRARY_PROJECT" -c $CONFIGURATION
 
-# build and run tests
+# build tests
 TEST_PROJECT=$_SCRIPT_DIR/src/IxMilia.Dxf.Test/IxMilia.Dxf.Test.csproj
 dotnet restore "$TEST_PROJECT"
-dotnet build "$TEST_PROJECT"
-dotnet test "$TEST_PROJECT"
+dotnet build "$TEST_PROJECT" -c $CONFIGURATION
+
+# run tests
+if [ "$RUNTESTS" = "true" ]; then
+    dotnet test "$TEST_PROJECT" -c $CONFIGURATION --no-restore --no-build
+fi
